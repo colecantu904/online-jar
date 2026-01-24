0d02e5b72034124af9f23d8f7376e00ae999d7e3
Cole Cantu <colecantu904@gmail.com>
2026-01-24 18:25:59 -0500

    working db operations

--- api/get-room.js
+++ api/get-room.js
@@ -1,15 +1,27 @@
 import { neon } from "@neondatabase/serverless"
 
+// figure out exactly how this should be made, how backend should be constructed
+// from memory, and then remake the whole thing by yourself with all the proper
+// routes and functions.
+
+// also need to learn sql to interact with the db, now that the connection is made!
+// make sure to give a timline on jars, they should delete themselves after so much
+// time of inactivity.
+
 export default async function handler(request, response) {
     try {
+        let testing_room_code = 'ABCDEF';
+        let testing_amount = 10;
         
+        // we are making a connection!!
         const db = neon(process.env.DATABASE_URL);
 
         // do some things to get the info from the request
+        await db`UPDATE jars SET jar_amount = jar_amount + ${testing_amount} WHERE jar_code = ${testing_room_code}`;
 
-        const data = json({room : 1});
+        const data = await db`SELECT jar_amount FROM jars WHERE jar_code = ${testing_room_code}`;
 
-        return response.status(200).data;
+        return response.status(200).json(data);
 
     } catch (error) {
         return response.status(500).json({ error: error.message });
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// we got a good amount of logic to add here...
+
+// for this, need to figure out how to interact with cookies in js
+// have to find if there is a jar stored at the code, otherwise if
+// there is no code, or else, you can input a code into a form
+// which will redirect to the query url.
+// you can then send the query url to freinds so that they can immediantly
+// navigate to it. Will I need users in order to track who is submitting to 
+// the jar?
 
 async function getFirstRoom( event ) {
     
@@ -5,6 +14,9 @@ async function getFirstRoom( event ) {
 
     try {
         const response = await fetch('/api/get-room');
+
+        console.log(response);
+
         const data = await response.json();
 
         console.log(data);
@@ -13,5 +25,7 @@ async function getFirstRoom( event ) {
         console.log("Error fetching data at /api/get-room", error);
     }
 }
+
+// async function addToJar( amount )
 
 window.getFirstRoom = getFirstRoom;
